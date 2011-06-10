ece71629f7ae9de5b6ced9295cff30f716bfc15b
Mark <markandrusroberts@gmail.com>
2011-06-10 07:11:49 -0500

    cleanup

--- kaprekar.hs
+++ kaprekar.hs
@@ -21,42 +21,31 @@ mkMaxMin n = (unDigits base $ List.reverse m, unDigits base m)
     where m = replicate (width - length l) 0 ++ l
           l = List.sort $ digitsRev base n
 
-
 genPairs = List.map (mkMaxMin . unDigits base) $ f width (base-1) where
     f 0 _ = [[]]
-    f k m = do
-            a <- [0..m]
-            ax <- f (k-1) a
-            if k==width && all (a==) ax
-               then []
-               else return $! a:ax
-
+    f k m = do a <- [0..m]
+               ax <- f (k-1) a
+               if k==width && all (a==) ax then []
+                                           else return $! a:ax
 
 subPairs = List.map (uncurry (-))
 
+regenPairs = List.map mkMaxMin
 
-regenPairs ps = filter (uncurry (/=)) --is this line necessary?
-               $ List.map mkMaxMin ps
-
-
-iterations xs | not . all (uncurry (==)) $ zip is js = iterations ys
-              | otherwise = ys
+iterations xs | all (uncurry (==)) $ zip is js = ys
+              | otherwise = iterations ys
      where ys = zip js qs
            js = List.map (\(i, p, q) -> if p==q then i else i+1) $ zip3 is ps qs
            qs = subPairs $ regenPairs ps
            (is, ps) = unzip xs
  
-
-lookupCoord :: Int -> [((Int, Int), Int)] -> Int
 lookupCoord n kvs | isNothing v = -1
                   | otherwise  = fromJust v where v = lookup (mkMaxMin n) kvs
-
 
 {- Image Functions -}
 
 isqrt = floor . sqrt . fromIntegral
 
-i, j, k :: Int
 (i, j, k) = (isqrt numbers, isqrt numbers, 4 {-RGBA-})
 
 -- TODO: rewrite this so we can accomodate numbers iteration steps larger than 6.
@@ -72,9 +61,7 @@ v ps = V.fromList . concat $ List.map (c . snd) ps
            c   6  = [255,   0,   0, 255] -- Red
            c   _  = [  0,   0,   0, 255] -- Black
 
-
 ptr2repa = copyFromPtrWord8 (Z :. i :. j :. k)
-
 
 {- Main -}
 
